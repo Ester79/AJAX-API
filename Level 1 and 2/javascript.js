9ff8b361c0e28bb95b9818c0c8e51f1d4dcb7811
Ester79 <esterramoscuervo@hotmail.com>
2021-06-07 11:30:16 +0200

    optimizacion2

--- Level 1 and 2/javascript.js
+++ Level 1 and 2/javascript.js
@@ -1,52 +1,57 @@
 
 
 var contadorcaras = 0;
-const appJoke = "http://api.icndb.com/jokes/random";
 
 
-//Cargar documento
-$(document).ready(function () {
 
 
-    $.getJSON(appJoke, function (dato, status, xhr) { 
-        console.log(dato.value.joke);
-
+//función llamada app
+function getJoke() {
+    $.getJSON("http://api.icndb.com/jokes/random", function (dato, status, xhr) {
         $("#acudit").html(dato.value.joke);
     });
+};
+
+
+
+
+//Función Cargar documento
+$(document).ready(function () {
+    getJoke();
 });
+
 
 //Función al hacer click en el botón
 $("#botonacudit").click(function () {
+    getJoke();
 
-    $.getJSON(appJoke, function (dato, status, xhr) { 
-        $("#acudit").html(dato.value.joke);
-        $("#smile").removeClass("nosmile");
-        $(".n1").addClass("smile");
-        let icono = $("#smile").html();
+    $("#smile").removeClass("nosmile");
+    $(".n1").addClass("smile");
+    let icono = $("#smile").html();
 
-        if (contadorcaras == 1) {
-            $("#smile").html("&#128527;");
-        } else if (contadorcaras == 2) {
-            $("#smile").html("&#128556;");
-        } else if (contadorcaras == 3) {
-            $("#smile").html("&#128518;");
+    if (contadorcaras == 1) {
+        $("#smile").html("&#128527;");
+    } else if (contadorcaras == 2) {
+        $("#smile").html("&#128556;");
+    } else if (contadorcaras == 3) {
+        $("#smile").html("&#128518;");
 
-        } else if (contadorcaras == 4) {
-            $("#smile").html("&#128517;");
-        } else if (contadorcaras == 5) {
-            $("#smile").html("&#128518;");
-        } else if (contadorcaras == 6) {
-            $("#smile").html("&#128514;");
-        } else if (contadorcaras > 6) {
-            $("#smile").html(icono + "&#128514;");
-        } else {
-            $("#smile").html("&#128578;");
-            
-        }
-        contadorcaras = contadorcaras + 1;
+    } else if (contadorcaras == 4) {
+        $("#smile").html("&#128517;");
+    } else if (contadorcaras == 5) {
+        $("#smile").html("&#128518;");
+    } else if (contadorcaras == 6) {
+        $("#smile").html("&#128514;");
+    } else if (contadorcaras > 6) {
+        $("#smile").html(icono + "&#128514;");
+    } else {
+        $("#smile").html("&#128578;");
 
-    });
+    }
+    contadorcaras = contadorcaras + 1;
+
 });
+
 
 
 
